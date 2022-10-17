89fda9dfacb9a36ad5210c6004e920b50714dbe4
MuhammedKasujja <al.kasmud.2@gmail.com>
2022-10-17 09:31:39 +0300

    feat: fetch and abort on using same url from server hook using axios

---
+++ src/Api/useFetchData.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import axios, { Method } from 'axios';
+
+export function useFetchData(url: string, type: Method) {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<any>()
+
+    useEffect(() => {
+        const controller = new AbortController()
+        const fetchData = async () => {
+            try {
+                const { data: response } = await axios.options(url, {
+                    method: type, signal: controller.signal,
+                });
+                setData(response);
+            } catch (error) {
+                setError(error)
+            } finally { setLoading(false) }
+        };
+
+        fetchData();
+        return () => {
+            controller.abort
+        }
+    }, [url]);
+
+    return {
+        data,
+        loading,
+        error,
+    };
+};
